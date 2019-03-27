35ab8216f1fff7f252101fc0101f925ee635a929
Quirky <48966267+Quirky1869@users.noreply.github.com>
2019-03-27 22:08:51 +0100

    Add files via upload

---
+++ get-adgroup.ps1
@@ -0,0 +1,6 @@
+﻿Get-ADGroup  -Filter "name -like 'G_ordinateurs*'" | foreach {
+    $nomgroupe = $_.name
+    [string[]]$listemembres = Get-ADGroupMember $nomgroupe | foreach {$_.name}
+    if ($listemembres -eq $null) {$nombre = 0} else {$nombre = $listemembres.count}
+    "" | select @{n="Groupe";e={$nomgroupe}}, @{n="Nombre";e={$nombre}}
+    } 
