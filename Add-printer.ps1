8e145ad42ed22e1b645407d298dc4c0d0556f5bb
Quirky <48966267+Quirky1869@users.noreply.github.com>
2019-03-30 20:56:42 +0100

    Add files via upload
    For add a printer on a computer from a server

---
+++ Add-printer.ps1
@@ -0,0 +1,13 @@
+﻿#==========================================================Add Printer=============================================================# 
+
+$PC = Read-Host "Entrez nom PC"
+
+$printer = "Server Name"
+
+$printer += Read-Host "Entrez le nom de l'imprimante a ajouter".ToUpper().Replace('a','A')
+
+Write-Host $printer
+
+Add-Printer -ComputerName $PC -ConnectionName $printer
+
+Get-Printer | Out-GridView
