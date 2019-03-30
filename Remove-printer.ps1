926c48d70127b33e873cd8a10b94b30bdc604dcb
Quirky <48966267+Quirky1869@users.noreply.github.com>
2019-03-30 21:36:25 +0100

    Add files via upload
    For remove a printer on computer from a server
    
    WARNING !!!!! Replace "####Enter server name HERE ####" by  your Server

---
+++ Remove-printer.ps1
@@ -0,0 +1,18 @@
+﻿#=========================================================Remove Printer==========================================================#
+
+$destinationLog = (Get-ChildItem Env:\USERPROFILE).value + "\Log-info-printer\$($printer).log"
+
+Start-Transcript -Path $destinationLog -Verbose -Append
+
+$pc = Read-Host "Enter computer name"
+
+$printer = "  ####Enter server name HERE ####   ".ToUpper()
+
+$printer += Read-Host "Enter the printer name to delete".ToUpper()
+
+Write-Host $printer
+
+Remove-Printer -Name $printer -ComputerName $pc
+
+
+Stop-Transcript
